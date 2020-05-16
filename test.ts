6d243254526d1b34ed92ddccf53635ba7e557c0f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-16 18:45:14 +0200

    🔴 computeGameState can mark the target as hit

--- test.ts
+++ test.ts
@@ -26,3 +26,35 @@ Deno.test({
     assertArrayContains(state.targets, [{ id: 0 }]);
   },
 });
+
+Deno.test({
+  name: "computeGameState can mark the target as hit",
+  fn(): void {
+    const pressedKeys: PressedKeys = {
+      down: false,
+      up: false,
+      left: false,
+      right: false,
+    }
+    const { id, key, timestamp } = computeGameState(pressedKeys).targets[0];
+    pressedKeys[key] = true;
+    const state = computeGameState(pressedKeys).targets[0];
+    assertArrayContains(state.targets, [{ id: 0, hit: true }]);
+  },
+});
+
+Deno.test({
+  name: "computeGameState returns another target, later in the game",
+  fn(): void {
+    const pressedKeys: PressedKeys = {
+      down: false,
+      up: false,
+      left: false,
+      right: false,
+    }
+    const state = computeGameState(pressedKeys);
+    assertArrayContains(state.targets, [{ id: 1 }]);
+  },
+});
+
+// TODO: computeGameState returns another target, later in the game
